1080c5ccfb0a6ac440da8a5e407b5fe830ef0d25
Travelle Whitney <travellewhitney@gmail.com>
2024-07-28 03:20:52 -0500

    Adds objectslab.md

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+
+const employee = {
+  name: 'Sam'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway");
+
+console.log(employee);
+console.log(updatedEmployee);
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  const streetAddress = {...'11 Broadway'};
+  employee[key] = value;
+  return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+  const updatedEmployee = {...employee}
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
